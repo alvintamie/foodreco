32fb49ce949ddb2f46ddfa7a19f0fea633fae0ea
Seivan Heidari <seivan@kth.se>
2012-10-02 16:02:54 +0800

    added forms and model partials

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -20,15 +20,25 @@ class MealsController < ApplicationController
   end
 
   def edit
-    
+    @survey = current_user.survey
+    @meal  = @survey.meals.find params[:id]
   end
 
   def update
-    
+    @survey = current_user.survey
+    @meal = @survey.meals.find params[:id] 
+    if @meal.update_attributes params[:meal]
+      redirect_to survey_meals_path(@survey)
+    else
+      render :new
+    end
   end
 
   def destroy
-    
+    @survey = current_user.survey
+    @meal = @survey.meals.find params[:id]
+    @meal.destroy
+    redirect_to survey_meals_path @survey
   end
 
 end
